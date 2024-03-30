d465c2a8e936c435ef616b6610bb20aa9559606b
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-03-30 12:52:05 +0000

    0.7.5
    - Fix for BSML 1.9.2

--- CustomSabers/Plugin.cs
+++ CustomSabers/Plugin.cs
@@ -13,7 +13,7 @@ namespace CustomSabersLite
     [Plugin(RuntimeOptions.SingleStartInit)]
     internal class Plugin
     {
-        private const string version = "0.7.4";
+        private const string version = "0.7.5";
 
         public static string Version { get { return version; } }
 
