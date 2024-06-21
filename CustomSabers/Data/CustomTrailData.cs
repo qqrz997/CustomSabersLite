6d677f4f069259210eed8a31ebf660d05fca391a
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2024-06-21 18:45:51 +0100

    Change trail data to record

--- CustomSabers/Data/CustomTrailData.cs
+++ CustomSabers/Data/CustomTrailData.cs
@@ -1,19 +1,8 @@
-﻿using CustomSabersLite.Utilities;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace CustomSabersLite.Data;
 
 /// <summary>
 /// Class that declares the neccessary information to create a <see cref="Components.Game.LiteSaberTrail"/>
 /// </summary>
-internal class CustomTrailData(Transform trailTop, Transform trailBottom, Material trailMaterial, Color trailColor, int length = TrailUtils.LegacyDuration)
-{
-    // Probably should attach an array of this to CustomSaberData and handle it when the saber is loaded so that we know preemptively if a saber has trails, or even has secondary trails
-
-    public Transform TrailTop { get; } = trailTop;
-    public Transform TrailBottom { get; } = trailBottom;
-    public Material Material { get; } = trailMaterial;
-    public Color Color { get; } = trailColor;
-    // public Color TrailColorMultiplier { get; }  todo ^
-    public int Length { get; } = length;
-}
+internal readonly record struct CustomTrailData(Transform Top, Transform Bottom, Material Material, CustomSaber.ColorType ColorType, Color Color, Color ColorMultiplier, float Length);
