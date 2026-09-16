11f077e7835b41c3fed8d645793cbf6a0dd4b359
qqrz997 <156676353+qqrz997@users.noreply.github.com>
2026-09-16 14:12:27 +0100

    unused

--- CustomSabers/Utilities/Extensions/SaberLoaderErrorExtensions.cs
+++ CustomSabers/Utilities/Extensions/SaberLoaderErrorExtensions.cs
@@ -9,7 +9,6 @@ internal static class SaberLoaderErrorExtensions
         SaberLoaderError.None => string.Empty,
         SaberLoaderError.InvalidFileType => "<color=#F77>Error - File is not of a valid type</color>",
         SaberLoaderError.FileNotFound => "<color=#F77>Error - Couldn't find a file</color>",
-        SaberLoaderError.LegacyWhacker => "<color=#F77>Not loaded - Legacy whacker, incompatible with PC</color>",
         SaberLoaderError.NullBundle => "<color=#F77>Error - Problem encountered when loading asset</color>",
         SaberLoaderError.NullAsset => "<color=#F77>Error - Problem encountered when loading saber model</color>",
         _ => "<color=#F77>Error - Unknown error encountered during loading</color>"
